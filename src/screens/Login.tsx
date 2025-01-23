9865f5627b3c5a6f9ebfac79e0da75117ac1bf10
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2025-01-24 00:37:38 +0100

    Request and access the temporary authorization code
    We provided Login component with callback for handling the authorization request,
    as well as an effect that handles the deeplinks with the temporary code, which
    implements the authorization grant from the GitHub service.

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,18 +1,50 @@
-import React, { JSX, useState } from 'react';
+import React, { JSX, useCallback, useEffect, useState } from 'react';
 import {
+  Linking,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 import { Button } from '../components/Button';
 
+const client_id = "Your clientID goes here";
+
 const Login = (): JSX.Element => {
   const [authCode, setAuthCode] = useState("");
   const [accessToken, setAccessToken] = useState("");
 
+  const handleAuthorization = useCallback(async () => {
+    const urlBase = "https://github.com/login/oauth/authorize";
+    const requestParams = {
+      client_id: client_id,
+      redirect_uri: "OAuthWithReactNativeTutorial://code",
+    }
+
+    const keys = Object.keys(requestParams);
+    const vals = Object.values(requestParams);
+    let reqParams = "";
+    for (let i = 0; i < keys.length; i++) {
+      reqParams += '&';
+      reqParams += `${keys[i]}=${vals[i]}`;
+    }
+    const reqUrl = urlBase + `?${reqParams}`;
+
+    await Linking.openURL(reqUrl);
+  }, []);
+
+  useEffect(() => {
+    Linking.addEventListener('url', ({ url }) => {
+      const temporaryToken = url.split("?")[1].split("=")[1];
+      setAuthCode(temporaryToken);
+    });
+    return () => {
+      Linking.removeAllListeners('url');
+    }
+  }, [setAuthCode]);
+
   return (
     <View style={styles.mainContainer}>
-      <Button onPress={() => {}} title='Authorize'/>
+      <Button onPress={handleAuthorization} title='Authorize'/>
       <View style={styles.tokenResultContainer}>
         <Text>Authorization code is:</Text>
         <Text>{authCode}</Text>
